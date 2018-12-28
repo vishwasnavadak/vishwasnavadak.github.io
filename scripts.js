939bc805e9487167671664c75e61809844a60a0d
Vishwas Navada <navadavishwas@hotmail.com>
2018-12-28 14:04:40 +0530

    === issue

--- scripts.js
+++ scripts.js
@@ -2,6 +2,6 @@
 $(document).ready(function(){
     $("#detail-switch").click(function(){
       $(".skill-content").toggle(300);
-      $(this).text($(this).text() === 'Show Me Details' ? 'Hide All Details!' : 'Show Me Details');
+      $(this).text($(this).text() === "Show Me Details" ? "Hide All Details!" : "Show Me Details");
     });
   });
