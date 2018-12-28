7e9b1a831d66ca6627521d21ac17680264adabd3
Vishwas Navada <navadavishwas@hotmail.com>
2018-12-28 13:33:39 +0530

    skill details toggle and others

---
+++ scripts.js
@@ -0,0 +1,7 @@
+/* Skill Details Toggle */
+$(document).ready(function(){
+    $("#detail-switch").click(function(){
+      $(".skill-content").toggle(300);
+      $(this).text($(this).text() == 'Show Me Details' ? 'Hide All Details!' : 'Show Me Details');
+    });
+  });
