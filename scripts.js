e12820d185f7d22180839fcc5eaeb307d5bc2106
Vishwas Navada <navadavishwas@hotmail.com>
2019-04-07 13:46:37 +0530

    Changed Content, Removed Progress Bar, Refactored HTML, Followed some best practises, fixed bootstrap issues, improved responsive design.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 /* Skill Details Toggle */
-$(document).ready(function(){
-    $("#detail-switch").click(function(){
-      $(".skill-content").toggle(300);
-      $(this).text($(this).text() === "Show Me Details" ? "Hide All Details!" : "Show Me Details");
-    });
+$(document).ready(function() {
+  $("#cur-year").html(new Date().getFullYear());
+  $("#detail-switch").click(function() {
+    $(".skill-content").toggle(300);
+    $(this).text(
+      $(this).text() === "Show Details" ? "Hide Details" : "Show Details"
+    );
   });
+});
